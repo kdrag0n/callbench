c2c7fbf6accad82893c08f177e553edd5117c50d
Danny Lin <danny@kdrag0n.dev>
2020-03-18 15:02:09 -0700

    Update copyright year

--- callbench.c
+++ callbench.c
@@ -8,7 +8,7 @@
  *
  * Licensed under the MIT License (MIT)
  *
- * Copyright (c) 2018-2019 Danny Lin <kdrag0n@pm.me>
+ * Copyright (c) 2018-2020 Danny Lin <kdrag0n@pm.me>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
