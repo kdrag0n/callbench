755465c583fc9ff1faaa4b25e360c3c91ac71ccc
Danny Lin <danny@kdrag0n.dev>
2020-07-15 17:33:12 -0700

    Convert to standard getopt argument parsing
    Some miscellaneous refactoring has also been done.

--- callbench.c
+++ callbench.c
@@ -40,6 +40,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <getopt.h>
 
 #define TEST_READ_PATH "/dev/zero"
 #define TEST_READ_LEN 65536
@@ -62,7 +63,7 @@ typedef void (*bench_impl)(void);
 
 static char test_read_buf[TEST_READ_LEN];
 
-static inline long ts_to_ns(struct timespec ts) {
+static long ts_to_ns(struct timespec ts) {
     return ts.tv_nsec + (ts.tv_sec * NS_PER_SEC);
 }
 
@@ -71,7 +72,7 @@ static void time_syscall_mb(void) {
     syscall(CLOCK_GETTIME_SYSCALL_NR, CLOCK_MONOTONIC, &ts);
 }
 
-static void time_implicit_mb(void) {
+static void time_libc_mb(void) {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
 }
@@ -96,17 +97,17 @@ static void file_mb(void) {
     close(fd);
 }
 
-static long run_bench_ns(bench_impl inner_call, unsigned long calls, unsigned long loops, unsigned long rounds) {
+static long run_bench_ns(bench_impl inner_call, int calls, int loops, int rounds) {
     long best_ns1 = LONG_MAX;
 
-    for (unsigned int round = 0; round < rounds; round++) {
+    for (int round = 0; round < rounds; round++) {
         long best_ns2 = LONG_MAX;
 
-        for (unsigned int loop = 0; loop < loops; loop++) {
+        for (int loop = 0; loop < loops; loop++) {
             struct timespec before;
             clock_gettime(CLOCK_MONOTONIC, &before);
 
-            for (unsigned int call = 0; call < calls; call++) {
+            for (int call = 0; call < calls; call++) {
                 inner_call();
             }
 
@@ -133,91 +134,129 @@ static long run_bench_ns(bench_impl inner_call, unsigned long calls, unsigned lo
     return best_ns1;
 }
 
-static unsigned long get_arg(int argc, char** argv, int index, unsigned long default_value) {
-    unsigned long value = 0;
-
-    if (argc > index)
-        value = atoi(argv[index]);
-    if (value == 0)
-        value = default_value;
-
-    return value;
+static int default_arg(int arg, int def) {
+    return arg == -1 ? def : arg;
 }
 
-int bench_time(int argc, char** argv) {
-    unsigned long calls = get_arg(argc, argv, 2, 100000);
-    unsigned long loops = get_arg(argc, argv, 3, 32);
-    unsigned long rounds = get_arg(argc, argv, 4, 5);
+static void bench_time(int calls, int loops, int rounds) {
+    calls = default_arg(calls, 100000);
+    loops = default_arg(loops, 32);
+    rounds = default_arg(rounds, 5);
 
     printf("clock_gettime: ");
     fflush(stdout);
 
     long best_ns_syscall = run_bench_ns(time_syscall_mb, calls, loops, rounds);
-    long best_ns_implicit = run_bench_ns(time_implicit_mb, calls, loops, rounds);
+    long best_ns_libc = run_bench_ns(time_libc_mb, calls, loops, rounds);
 
     printf("\n    syscall:\t%ld ns\n", best_ns_syscall);
-    printf("    libc:\t%ld ns\n", best_ns_implicit);
-
-    return 0;
+    printf("    libc:\t%ld ns\n", best_ns_libc);
 }
 
-int bench_file(int argc, char** argv) {
-    unsigned long calls = get_arg(argc, argv, 2, 100);
-    unsigned long loops = get_arg(argc, argv, 3, 128);
-    unsigned long rounds = get_arg(argc, argv, 4, 5);
+static void bench_file(int calls, int loops, int rounds) {
+    calls = default_arg(calls, 100);
+    loops = default_arg(loops, 128);
+    rounds = default_arg(rounds, 5);
 
     printf("file read: ");
     fflush(stdout);
 
     long best_ns_mmap = run_bench_ns(mmap_mb, calls, loops, rounds);
-    long best_ns_file = run_bench_ns(file_mb, calls, loops, rounds);
+    long best_ns_read = run_bench_ns(file_mb, calls, loops, rounds);
 
     printf("\n    mmap:\t%ld ns\n", best_ns_mmap);
-    printf("    read:\t%ld ns\n", best_ns_file);
+    printf("    read:\t%ld ns\n", best_ns_read);
+}
 
-    return 0;
+static char *short_options = "hm:c:l:r:";
+static struct option long_options[] = {
+    {"help", no_argument, 0, 'h'},
+    {"mode", required_argument, 0, 'm'},
+    {"calls", required_argument, 0, 'c'},
+    {"loops", required_argument, 0, 'l'},
+    {"rounds", required_argument, 0, 'r'},
+    {}
+};
+
+static void print_help(char *prog_name) {
+    printf("Usage: %s [options]\n"
+        "\n"
+        "This program benchmarks some simple kernel syscalls:\n"
+        "  Time: clock_gettime(CLOCK_MONOTONIC) with direct syscalls and libc wrapper calls\n"
+        "  File: reads 64 KiB of data from /dev/zero with mmap(2) and read(2)\n"
+        "\n"
+        "libc time calls may be faster than direct syscalls on some platforms due to\n"
+        "special fast paths without context switching, e.g. Linux's vDSO.\n"
+        "\n"
+        "Options:\n"
+        "  -h, --help\tshow usage help and exit\n"
+        "  -m, --mode\ttests to run: time, file, or all (default: all)\n"
+        "  -c, --calls\tnumber of syscalls to make per loop (default: 100000 for time, 100 for file)\n"
+        "  -l, --loops\tnumber of loops to run per round (default: 32 for time, 128 for file)\n"
+        "  -r, --rounds\tnumber of benchmark rounds to run (default: 5)\n",
+        prog_name);
+
+    exit(1);
+}
+
+static void parse_args(int argc, char **argv, bool *do_time, bool *do_file, int *calls, int *loops, int *rounds) {
+    while (1) {
+        int c = getopt_long(argc, argv, short_options, long_options, NULL);
+        if (c == -1)
+            break;
+
+        switch (c) {
+        case '?':
+        case 'h':
+            print_help(argv[0]);
+            break;
+        case 'm':
+            if (!strcmp(optarg, "time")) {
+                *do_time = 1;
+                *do_file = 0;
+            } else if (!strcmp(optarg, "file")) {
+                *do_time = 0;
+                *do_file = 1;
+            } else if (!strcmp(optarg, "all")) {
+                *do_time = 1;
+                *do_file = 1;
+            } else {
+                fprintf(stderr, "%s: invalid mode -- '%s'\n", argv[0], optarg);
+                print_help(argv[0]);
+            }
+            break;
+        case 'c':
+            *calls = atoi(optarg);
+            break;
+        case 'l':
+            *loops = atoi(optarg);
+            break;
+        case 'r':
+            *rounds = atoi(optarg);
+            break;
+        }
+    }
 }
 
 int main(int argc, char** argv) {
-    int ret;
-    bool do_time = 0;
-    bool do_file = 0;
+    bool do_time = 1;
+    bool do_file = 1;
+    int calls = -1;
+    int loops = -1;
+    int rounds = -1;
 
-    char mode = 'a';
-    if (argc >= 2) { // 1+ arguments
-        mode = tolower(argv[1][0]); // First letter of 1st argument
-        if (mode != 't' && mode != 'f' && mode != 'a') {
-            fprintf(stderr, "Invalid mode '%c'! Valid modes are: [t]ime, [f]ile, [a]ll\n", mode);
-            return 1;
-        }
-    }
-
-    switch (mode) {
-    case 't':
-        do_time = 1;
-        break;
-    case 'f':
-        do_file = 1;
-        break;
-    case 'a':
-        do_time = 1;
-        do_file = 1;
-        break;
-    }
+    parse_args(argc, argv, &do_time, &do_file, &calls, &loops, &rounds);
 
     if (do_time) {
-        ret = bench_time(argc, argv);
-        if (ret)
-            return ret;
+        bench_time(calls, loops, rounds);
     }
 
-    if (do_time && do_file)
+    if (do_time && do_file) {
         putchar('\n');
+    }
 
     if (do_file) {
-        ret = bench_file(argc, argv);
-        if (ret)
-            return ret;
+        bench_file(calls, loops, rounds);
     }
 
     return 0;
